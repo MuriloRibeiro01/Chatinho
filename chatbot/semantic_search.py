e9d1cc36b9709c7e3f51e8927872e1b7584f85ba
MuriloRibeiro01 <murilo.ribeiro2709@gmail.com>
2026-05-27 00:17:29 -0300

    feat: Criação de busca semântica utilizando vetor do embedding.

---
+++ chatbot/semantic_search.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+from embeddings import gerar_embedding
+from tasks import carregar_tarefas
+
+def buscar_tarefas_semelhantes(texto, limite=3, treshold=0.55):
+    
+    tarefas = carregar_tarefas()
+
+    if not tarefas:
+        return []
+    
+    consulta_embedding = gerar_embedding(texto)
+
+    resultados = []
+
+    for tarefa in tarefas:
+
+        if "embedding" not in tarefa:
+            continue
+
+        # Acha similaridade com base nos vetores da tarefa
+        similaridade = cosine_similarity(
+            [consulta_embedding],
+            [tarefa["embedding"]]
+        )[0][0]
+
+        if similaridade >= treshold:
+
+            # Adiciona contexto para a tarefa com base na similaridade
+            resultados.append({
+                "tarefa": tarefa,
+                "similaridade": float(similaridade)
+            })
+    
+    # Ele organiza pela similaridade em ordem decrescente.
+    resultados.sort(
+        key=lambda x: x["similaridade"],
+        reverse=True
+    )
+
+    return resultados[:limite]
